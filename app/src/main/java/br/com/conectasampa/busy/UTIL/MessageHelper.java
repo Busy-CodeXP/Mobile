634d8d5858093a56a7c8ae9f5d29287393423d09
dfalex <dani_falex@hotmail.com>
2018-03-06 21:40:09 -0300

    first commit

---
+++ app/src/main/java/br/com/conectasampa/busy/UTIL/MessageHelper.java
@@ -0,0 +1,17 @@
+package br.com.conectasampa.busy.UTIL;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+
+/**
+ * Created by GIS on 06/03/2018.
+ */
+
+public class MessageHelper {
+    public static void MostraMensagem(Activity act, String titulo, String mensagem){
+        new AlertDialog.Builder(act)
+                .setTitle(titulo)
+                .setMessage(mensagem)
+                .setCancelable(true).show();
+    }
+}
